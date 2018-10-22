13b8ee713b96702d55a98017fc3b0d7847f11b18
Adrian <sci.adrian@gmail.com>
2018-10-22 22:46:32 +0300

    Updated tests

--- CustomerService.Tests/CostumerRepositoryTests.cs
+++ CustomerService.Tests/CostumerRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using CustomerService.Interfaces;
 using CustomerService.Models;
 using CustomerService.Repositories;
@@ -12,7 +13,7 @@ namespace CustomerService.Tests
     public class CostumerRepositoryTests
     {
         private Mock<IDataProvider> dataProviderMock;
-        private CustomerRepository customerRepository;
+        private static CustomerRepository customerRepository;
 
         [SetUp]
         public void Init()
@@ -34,7 +35,7 @@ namespace CustomerService.Tests
             customerRepository = new CustomerRepository(dataProviderMock.Object);
 
             // Act
-            var data = customerRepository.GetAllCustomers().Result;
+            var data = customerRepository.GetAllCustomers().Result.ToList();
 
             // Assert
             Assert.That(data, Is.EqualTo(expectedCustomers));
@@ -50,11 +51,46 @@ namespace CustomerService.Tests
             customerRepository = new CustomerRepository(dataProviderMock.Object);
 
             // Act
-            var data = customerRepository.GetAllCustomers().Result;
+            var data = customerRepository.GetAllCustomers().Result.ToList();
 
             // Assert
             Assert.That(data, Is.Empty);
             Assert.That(data.Count(), Is.Zero);
+        }
+
+        [Test]
+        public void DeleteCustomerByIdShouldRemoveCustomerFromList()
+        {
+            // Arrange
+            var customersList = new List<Customer>()
+            {
+                new Customer(1, "test", "test", "test", "test", "test"),
+                new Customer(2, "test", "test", "test", "test", "test"),
+                new Customer(3, "test", "test", "test", "test", "test")
+            };
+            var expectedData = new List<Customer>()
+            {
+                new Customer(1, "test", "test", "test", "test", "test"),
+                new Customer(3, "test", "test", "test", "test", "test")
+            };
+            dataProviderMock.Setup(s => s.InitializeCustomers()).ReturnsAsync(customersList);
+            customerRepository = new CustomerRepository(dataProviderMock.Object);
+
+            // Act
+            customerRepository.DeleteCustomer(2);
+            var actualData = customerRepository.GetAllCustomers().Result.ToList();
+
+            // Assert
+            Assert.That(actualData.Count, Is.EqualTo(expectedData.Count));
+            for (int i = 0; i < actualData.Count(); i++)
+            {
+                Assert.That(actualData[i].Id, Is.EqualTo(expectedData[i].Id));
+                Assert.That(actualData[i].Name, Is.EqualTo(expectedData[i].Name));
+                Assert.That(actualData[i].Email, Is.EqualTo(expectedData[i].Email));
+                Assert.That(actualData[i].IBAN, Is.EqualTo(expectedData[i].IBAN));
+                Assert.That(actualData[i].Phone, Is.EqualTo(expectedData[i].Phone));
+                Assert.That(actualData[i].Suffix, Is.EqualTo(expectedData[i].Suffix));
+            }
         }
     }
 }
